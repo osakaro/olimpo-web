6ad3ea79d498f755d25ee3cd7510c04040df54d3
osakaro <oskrolgames@gmail.com>
2026-03-09 20:09:51 +0000

    Añadir barra menu para móviles

--- public/interaccion.js
+++ public/interaccion.js
@@ -459,3 +459,8 @@ window.onload = () => {
     configurarEnvioLogin();
     configurarEnvioRegistro();
 };
+
+function toggleMobileMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+}
